b1508ff09afb62b7374bf8000a12866fb9813ebc
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2025-02-01 16:27:21 -0800

    feat(ui): Created info tiles for various timesheets

---
+++ client/src/components/TimesheetCard/TimesheetCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './TimesheetCard.module.css';
+
+export default function TimesheetCard({ timesheetObject }) {
+    return ( 
+        <div className={styles.cardCtn}> 
+            <div className={styles.iconTile}>
+                <img src="/document.svg" alt="document" />
+                <div className={styles.metadata}>
+                    <p> { timesheetObject.name } </p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,11 +1,22 @@
 import React from "react";
 import styles from "./HomePage.module.css";
 import SideMenu from "../../components/SideMenu/SideMenu";
+import TimesheetCard from "../../components/TimesheetCard/TimesheetCard";
 
 export default function HomePage() { 
+    const testObject = { name: "Timesheet 1" }; 
+
     return (
-        <div>
+        <div className={styles.parentCtn}>
             <SideMenu />
+
+            <TimesheetCard timesheetObject={testObject} />
+            <TimesheetCard timesheetObject={testObject} />
+            <TimesheetCard timesheetObject={testObject} />
+            <TimesheetCard timesheetObject={testObject} />
+            <TimesheetCard timesheetObject={testObject} />
+            
+
         </div>
     )
 }
