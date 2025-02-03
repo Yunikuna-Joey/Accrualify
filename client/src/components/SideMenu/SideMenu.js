24a9ed80e3d8b64b878b8d66417855d0a9923fcb
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2025-02-02 23:00:23 -0800

    fix(ui): Made input boxes slightly bigger
    - made sidebar menu close by default

--- client/src/components/SideMenu/SideMenu.js
+++ client/src/components/SideMenu/SideMenu.js
@@ -6,7 +6,7 @@ import { UserContext } from '../../context/UserContext';
 
 
 export default function SideMenu() { 
-    const [ isOpen, setIsOpen ] = useState(true);
+    const [ isOpen, setIsOpen ] = useState(false);
     
     const toggleSidebar = () => { 
         setIsOpen(!isOpen);
